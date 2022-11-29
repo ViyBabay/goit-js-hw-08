291149c3c8592cb1953b58ccd05b5408cbd2f9a2
Oleksandr Pikush <pikush.oleksandr@gmail.com>
2022-11-29 22:02:32 +0200

    task 2 red

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const vimeoEl = document.querySelector('iframe');
+const player = new Player(vimeoEl);
 const timer = 'videoplayer-current-time';
 
 
