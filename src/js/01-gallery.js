b3e662589278a4d4165ab8f7f2cc2438d0c3a6ad
Oleksandr Pikush <pikush.oleksandr@gmail.com>
2022-11-29 17:33:02 +0200

    task 1 + 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const newGallery = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItemsMarkupCreate(galleryItems);
+
+newGallery.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+function galleryItemsMarkupCreate(pics) {
+    return pics
+        .map((pic) => 
+    `<a class="gallery__item" href="${pic.original}">
+    <img class="gallery__image"
+    src="${pic.preview}" 
+    alt="${pic.description}" />
+    </a>`
+    )
+    .join("");
+}
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timer = 'videoplayer-current-time';
+
+
+player.on(
+    'timeupdate',
+    throttle(evt => {
+      localStorage.setItem(timer, evt.seconds);
+    }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem(timer) || 0);
